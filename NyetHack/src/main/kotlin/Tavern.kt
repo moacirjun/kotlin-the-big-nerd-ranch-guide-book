688f7befe9dd01c5a07731624d46641cdf32203a
moacirjun <moacirjunior.eletro@gmail.com>
2021-08-17 00:00:39 -0300

    Chapter 7.

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -1,13 +1,37 @@
-fun main() {
-    var beverage = readLine()
-//    beverage = null
+const val TAVERN_NAME = "Taernyl's Folly"
 
-    if (beverage != null) {
-        beverage = beverage.capitalize()
+fun main() {
+    placeOrder("Bebida,DrAgon's Breath,5.12")
+}
+
+fun placeOrder(menu: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menu.split(',')
+    val message = "Madrigal buys a $name ($type) for $price mangos."
+
+    println(message)
+
+    val phrase = if (name == "DrAgon's Breath") {
+        "Madrigal exclaims: ${toDragonSpeak("Ah, delicious $name")}"
     } else {
-        println("I can't perform that. The beverage is null!")
+        "Madrigal says: Thanks for the $name"
     }
 
-    val beverageServed = beverage ?: "Buttered Ale"
-    println(beverageServed)
+    println(phrase)
 }
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[a-uA-U]")) {
+        when(it.value) {
+            in listOf("a", "A") -> "4"
+            in listOf("e", "E") -> "3"
+            in listOf("i", "I") -> "1"
+            in listOf("o", "O") -> "0"
+            in listOf("u", "U") -> "|_|"
+            else -> it.value
+        }
+    }
