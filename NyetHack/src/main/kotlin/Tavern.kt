c9350457b60ecc8e98653621c057f10001b265da
moacirjun <moacirjunior.eletro@gmail.com>
2021-08-29 12:59:49 -0300

    Chapter 8.

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -1,7 +1,32 @@
+import kotlin.math.roundToInt
+
 const val TAVERN_NAME = "Taernyl's Folly"
+
+var playerGold = 10
+var playerSilver = 10
 
 fun main() {
     placeOrder("Bebida,DrAgon's Breath,5.12")
+}
+
+fun performPurchase(price: Double) {
+    displayBalance()
+
+    val totalPurse = playerGold + (playerSilver / 100.0)
+
+    println("Total purse: $totalPurse")
+    println("Purchasing item for $price")
+
+    val remainingBalance = totalPurse - price
+    println("Remaining balance: ${"%.2f".format(remainingBalance)}")
+
+    playerGold = remainingBalance.toInt()
+    playerSilver = (remainingBalance % 1 * 100).roundToInt()
+    displayBalance()
+}
+
+private fun displayBalance() {
+    println("Player's purse balance: Gold $playerGold, Silver $playerSilver.")
 }
 
 fun placeOrder(menu: String) {
@@ -14,6 +39,7 @@ fun placeOrder(menu: String) {
     val message = "Madrigal buys a $name ($type) for $price mangos."
 
     println(message)
+    performPurchase(price.toDouble())
 
     val phrase = if (name == "DrAgon's Breath") {
         "Madrigal exclaims: ${toDragonSpeak("Ah, delicious $name")}"
